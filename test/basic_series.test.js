8a37ddbb9c485b83302432a8889c530e469a6f38
LynxyssCZ <LynxyssCZ@gmail.com>
2018-04-05 13:00:22 +0200

    Add sequential tests to series as well

--- test/basic_series.test.js
+++ test/basic_series.test.js
@@ -5,12 +5,27 @@ const Mapromise = require('../series');
 describe('Series', function() {
 	const input = [3,2,4,1,5,6,7,4];
 
+  const identityCallback = (value) => {
+		return new Promise((resolve) => {
+			global.setTimeout(() => {
+				resolve(value);
+			}, value);
+		});
+	};
+
 	const indexSensitiveCallback = (value, index) => {
 		return value - index;
 	};
 
 	context('Basic iteration', function() {
-		it('should be fully serial', function() {
+    it('results should be sequential', function() {
+			return Mapromise(input, identityCallback)
+				.then((res) => {
+					expect(res).eql(input);
+				});
+		});
+
+		it('execution should be fully serial', function() {
 			const output = [];
 			const identityCallback = (value) => {
 				return new Promise((resolve) => {
